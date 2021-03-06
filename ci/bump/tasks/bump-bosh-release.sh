9c5e01ec9c752476da32258d7a8c699225b3011f
Benjamin Gandon <benjamin@gstack.io>
2021-03-06 20:40:48 +0100

    Add Concourse task for bumping a BOSH Release version in a Turbine Subsys spec file

---
+++ ci/bump/tasks/bump-bosh-release.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail -x
+
+function main() {
+    install_yasak
+
+    local version sha1
+    version=$(< bosh-io-release/version)
+    sha1=$(< bosh-io-release/sha1)
+
+    git clone "git" "git-bumped"
+
+    pushd "git-bumped" > /dev/null
+
+        update-yaml-value \
+            "${SPEC_FILE}" \
+            "/deployment_vars/${YAML_PROP_PREFIX}_version" \
+            "\"${version}\""
+
+        update-yaml-value \
+            "${SPEC_FILE}" \
+            "/deployment_vars/${YAML_PROP_PREFIX}_sha1" \
+            "${sha1}"
+
+        make_commit
+    popd > /dev/null
+}
+
+function make_commit() {
+    git config --global "color.ui" "always"
+    git status
+    git diff | cat
+
+    git config --global "user.name" "((git-commit-name))"
+    git config --global "user.email" "((git-commit-email))"
+
+    if [[ -z "$(git status --porcelain)" ]]; then
+        echo "INFO: nothing to commit. Skipping."
+    else
+        git add .
+        git commit -m "Bump ${RELEASE_HUMAN_NAME} to version ${version}"
+    fi
+}
+
+function install_yasak() {
+    go get github.com/gstackio/yasak
+    sed -i 's|sed -e|sed -i -e|' "yasak/example-edit-value.sh"
+    cp "yasak/example-edit-value.sh" "/usr/local/bin/update-yaml-value"
+}
+
+main "$@"
